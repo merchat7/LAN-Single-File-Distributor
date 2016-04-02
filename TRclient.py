facfd2f5c13e00e49deeaef71df48660a8388544
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2016-04-02 22:42:02 +0700

    Quick test of libtorrent downloading and seeding a .torrent file using pre-existing code.

---
+++ TRclient.py
@@ -0,0 +1,35 @@
+import libtorrent as lt
+import time,sys
+
+ses = lt.session()
+ses.listen_on(6881, 6891)
+
+e = lt.bdecode(open("test.torrent", 'rb').read())
+info = lt.torrent_info(e)
+h = ses.add_torrent({'ti': info, 'save_path': 'C:\Users\merchat7\Desktop'})
+print 'starting', h.name()
+
+while (not h.is_seed()):
+   s = h.status()
+
+   state_str = ['queued', 'checking', 'downloading metadata', \
+      'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
+   print '\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+      (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
+      s.num_peers, state_str[s.state]),
+   sys.stdout.flush()
+
+   time.sleep(1)
+
+print h.name(), 'complete'
+
+while h.is_seed():
+    s = h.status()
+    state_str = ['queued', 'checking', 'downloading metadata', \
+      'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
+
+    print('\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
+      (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
+    sys.stdout.flush()
+
+    time.sleep(1)
