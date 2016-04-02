8521edfb2cfcd7aa5ffac81c081e473c45c5dfa0
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2016-04-03 01:02:30 +0700

    Readd necessary code.

--- TRclient.py
+++ TRclient.py
@@ -25,11 +25,10 @@ def download():
     h = ses.add_torrent({'ti': info, 'save_path': 'C:\Users\merchat7\Desktop'})
     print 'starting', h.name()
 
-    s = h.status()
-    state_str = ['queued', 'checking', 'downloading metadata', \
-                 'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
-
     while (not h.is_seed()):
+       s = h.status()
+       state_str = ['queued', 'checking', 'downloading metadata', \
+                     'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
        print '\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
           (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, \
           s.num_peers, state_str[s.state]),
@@ -40,6 +39,9 @@ def download():
     print h.name(), 'complete'
 
     while h.is_seed():
+        s = h.status()
+        state_str = ['queued', 'checking', 'downloading metadata', \
+                     'downloading', 'finished', 'seeding', 'allocating', 'checking fastresume']
         print('\r%.2f%% complete (down: %.1f kb/s up: %.1f kB/s peers: %d) %s' % \
           (s.progress * 100, s.download_rate / 1000, s.upload_rate / 1000, s.num_peers, state_str[s.state]))
         sys.stdout.flush()
