2e29bdc69d857c27f0e2ef903c6d57ab480992e5
hart_suvanadat@hotmail.com <hart_suvanadat@hotmail.com>
2016-04-03 16:10:10 +0700

    Download to user download's directory and add all interfaces IP as possible trackers.

--- TRclient.py
+++ TRclient.py
@@ -1,29 +1,44 @@
+import sys
+import time
+from netifaces import interfaces, ifaddresses, AF_INET
+from os.path import expanduser
+
 import libtorrent as lt
-import time,sys
+
+
+#from http://stackoverflow.com/questions/270745/how-do-i-determine-all-of-my-ip-addresses-when-i-have-multiple-nics
+def ip4_addresses():
+    ip_list = []
+    for interface in interfaces():
+        for link in ifaddresses(interface).get(AF_INET, ()):
+            ip_list.append(link['addr'])
+    return ip_list
+
 
 def maketorrent():
     fs = lt.file_storage()
     lt.add_files(fs, "test.mp3")
     t = lt.create_torrent(fs)
-    t.add_tracker("http://192.168.1.100:6969/announce", 0)
-    t.add_tracker("http://127.0.0.1:6969/announce",0)
-    t.add_tracker("http://0.0.0.0:6969/announce", 0)
+    for ip in ip4_addresses():
+        trAddress = "http://" + ip + ":6969/announce"
+        t.add_tracker(trAddress,0)
     t.set_creator('A bunny')
     lt.set_piece_hashes(t, ".")
     torrent = t.generate()
-    f = open("mytorrent.torrent", "wb")
+    f = open("t.torrent", "wb")
     f.write(lt.bencode(torrent))
     f.close()
 
 #maketorrent()
 
 def download():
+    home = expanduser("~")
     ses = lt.session()
     ses.listen_on(6881, 6891)
 
-    e = lt.bdecode(open("mytorrent.torrent", 'rb').read())
+    e = lt.bdecode(open("t.torrent", 'rb').read())
     info = lt.torrent_info(e)
-    h = ses.add_torrent({'ti': info, 'save_path': 'C:\Users\merchat7\Desktop'})
+    h = ses.add_torrent({'ti': info, 'save_path': home + "\Downloads"})
     print 'starting', h.name()
 
     while (not h.is_seed()):
--- TRtracker.py
+++
@@ -1,9 +0,0 @@
-from netifaces import interfaces, ifaddresses, AF_INET
-def ip4_addresses():
-    ip_list = []
-    for interface in interfaces():
-        for link in ifaddresses(interface).get(AF_INET, ()):
-            ip_list.append(link['addr'])
-    return ip_list
-
-print ip4_addresses()
