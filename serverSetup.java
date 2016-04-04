01d97788faff4f6ee796e95a733012a3a63aba78
dodinhtrung97 <dodinhtrung97@gmail.com>
2016-04-04 13:31:08 +0700

    Add progress database

--- serverSetup.java
+++ serverSetup.java
@@ -1,16 +1,28 @@
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletHandler;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class serverSetup implements Runnable {
+
+    static ArrayList<String> data = new ArrayList<>();
 
     protected int serverPort = 8080;
     protected ServerSocket serverSocket = null;
     protected boolean isStopped = false;
     protected Thread runningThread = null;
+
+    BufferedReader in = null;
+    String inputLine;
+    String toAdd;
 
     public serverSetup(int port) throws Exception {
 
@@ -34,7 +46,7 @@ public class serverSetup implements Runnable {
         handler.addServletWithMapping(Servlet.class, "/*");
     }
 
-    public void run() {
+    public void run(){
         synchronized (this) {
             this.runningThread = Thread.currentThread();
         }
@@ -56,6 +68,31 @@ public class serverSetup implements Runnable {
             }
             /* Start worker thread on new client */
             new Thread(new WorkerRunnable(clientSocket, "Multithreaded Server")).start();
+
+            try {
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())); // Buffer reader to receive possible progress report
+            } catch (IOException e) { e.printStackTrace(); }
+
+            try {
+                /* Each progress report will be one string */
+                inputLine = in.readLine();
+
+                if (inputLine.length() != 0){
+                    toAdd = inputLine; //Read until the end
+                }
+
+                String[] split = toAdd.split(",");
+
+                /* If progress report == Done */
+                if (split[1].equals("Done")){
+                    for (String i:data){ //Search for progress is database
+                        if (i.contains(split[1])){
+                            data.remove(i); //Remove progress
+                        }
+                    }
+                } else { data.add(toAdd); /* Add progress to database */ }
+
+            } catch (IOException e) { e.printStackTrace(); }
         }
         System.out.println("Server Stopped.");
     }
