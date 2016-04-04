7ba8f41af1fa9573621a8466f2cc907e9de68a48
dodinhtrung97 <dodinhtrung97@gmail.com>
2016-04-04 09:06:31 +0700

    Server setup, handles multithread connections but not multithreads download

---
+++ Servlet.java
@@ -0,0 +1,54 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/* Servlet class handles file management and sending files */
+public class Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String param = request.getParameter("p");
+
+        /* If false request */
+        if (param == null)
+        {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Parameter p missing");
+            return;
+        }
+
+        /* Create buffer to read */
+        int bufferSize = 1024;
+        byte[] buffer = new byte[bufferSize];
+
+        /* Assume file is inside current dir */
+        File fileTorrent = new File("test.torrent");
+        FileInputStream input = new FileInputStream(fileTorrent);
+        String filename = "test";
+
+        /* Prep work for response parameter */
+        response.setContentType("text/csv");
+        response.setHeader("Content-disposition", "attachment; filename=\""+filename+"\"");
+        response.setContentLength((int) fileTorrent.length());
+
+        OutputStream output = response.getOutputStream();
+
+        /* Write and send in bytes */
+        try {
+            int byteRead;
+            while ((byteRead = input.read()) != -1) {
+                output.write(buffer, 0, byteRead);
+            }
+            output.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            output.close();
+            input.close();
+        }
+    }
+}
---
+++ createServer.java
@@ -0,0 +1,86 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.servlet.ServletHandler;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class createServer implements Runnable {
+
+    protected int serverPort = 8080;
+    protected ServerSocket serverSocket = null;
+    protected boolean isStopped = false;
+    protected Thread runningThread = null;
+
+    public void Server(int port) throws Exception {
+
+        this.serverPort = port;
+
+        /* Start server */
+        Server server = new Server(8080);
+
+        /* Setup HTTP Connector */
+        ServerConnector http = new ServerConnector(server);
+        http.setHost("localhost");
+        http.setPort(8080);
+        http.setIdleTimeout(30000);
+
+        /* Set connector */
+        server.addConnector(http);
+
+        /* Setup Servlet Handler */
+        ServletHandler handler = new ServletHandler();
+        server.setHandler(handler);
+        handler.addServletWithMapping(Servlet.class, "/*");
+    }
+
+    public void run() {
+        synchronized (this) {
+            this.runningThread = Thread.currentThread();
+        }
+
+        openServerSocket();
+
+        /* If server is not closed */
+        while (!isStopped()) {
+            Socket clientSocket;
+            try {
+                clientSocket = this.serverSocket.accept(); //Accept client
+            } catch (IOException e) {
+                /* If server closed */
+                if (isStopped()) {
+                    System.out.println("Server Stopped.");
+                    return;
+                }
+                throw new RuntimeException("Can't accept client");
+            }
+            /* Start worker thread on new client */
+            new Thread(new WorkerRunnable(clientSocket, "Multithreaded Server")).start();
+        }
+        System.out.println("Server Stopped.");
+    }
+
+    private synchronized boolean isStopped() {
+        return this.isStopped;
+    }
+
+    public synchronized void stop() {
+        this.isStopped = true;
+        try {
+            this.serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error closing server", e);
+        }
+    }
+
+    private void openServerSocket() {
+        try {
+            this.serverSocket = new ServerSocket(this.serverPort);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot open port 8080", e);
+        }
+    }
+}
+
+
+
